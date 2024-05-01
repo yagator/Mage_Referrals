abc8bb95b37a90962b7b6d1d8398d2f4c0eb3072
Yair García <yagator@gmail.com>
2024-04-30 23:51:42 -0600

    Added sample data installer

---
+++ app/code/Yagator/Referrals/Setup/Patch/Data/Sample.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Yagator\Referrals\Setup\Patch\Data;
+
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchInterface;
+use Magento\Framework\Setup\Patch\PatchRevertableInterface;
+use Magento\Framework\Filesystem\Driver\File;
+use Magento\Framework\File\Csv;
+
+
+class Sample implements DataPatchInterface, PatchRevertableInterface
+{
+
+    const CSV_FILE = 'sample_referrals.csv';
+
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    protected $moduleDataSetup;
+    /**
+     * @var Csv
+     */
+    protected $csv;
+    /**
+     * @var File
+     */
+    protected $filesystem;
+
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        Csv $csv,
+        File $filesystem
+    ){
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->csv = $csv;
+        $this->filesystem = $filesystem;
+    }
+
+    /**
+     * @return string[]
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+
+    /**
+     * @return $this
+     */
+    public function apply()
+    {
+        $connection = $this->moduleDataSetup->getConnection();
+        $connection->startSetup();
+
+        $sample_data = $this->getSampleData();
+        if ($sample_data !== false){
+            $table = $connection->getTableName('referral');
+            $fields = [];
+            foreach ($sample_data as $data) {
+                if (count($fields) == 0){
+                    $fields = $data;
+                } else {
+                    $insert_data = [];
+                    foreach ($fields as $key=>$field) {
+                        $insert_data[$field] = $data[$key];
+                    }
+                    $connection->insert($table, $insert_data);
+                }
+            }
+        } else {
+            echo 'CSV file does not exists or is empty';
+        }
+
+        $connection->endSetup();
+    }
+
+    /**
+     * @return void
+     */
+    public function revert()
+    {
+        // TODO: Implement revert() method.
+    }
+
+    private function getSampleData()
+    {
+        $file = __DIR__ . '/' . self::CSV_FILE;
+        if ($this->filesystem->isExists($file)) {
+            $this->csv->setDelimiter(',');
+            return $this->csv->getData($file);
+        }
+
+        return false;
+    }
+}
